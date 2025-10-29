f0887dacf106766f4c1321dc41886a7fd9162015
juffww <baoho1503@gmail.com>
2025-10-29 16:02:14 +0700

    add file package

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const vpnPackages = [
     memory: "3 MB",
     networkType: "Wifi or LAN",
     description: "Secure VPN client for Windows platform.",
-    file: "/packages/vpn-windows.zip",
+    // file: "/packages/vpn-windows.zip",
+    file: "/packages/note.txt",
   },
   {
     id: "mac",
@@ -31,7 +32,8 @@ const vpnPackages = [
     memory: "35 MB",
     networkType: "Wifi or LAN",
     description: "Secure VPN client for macOS platform.",
-    file: "/packages/vpn-macos.zip",
+    // file: "/packages/vpn-macos.zip",
+    file: "/packages/instruction.txt",
   },
 ];
 
